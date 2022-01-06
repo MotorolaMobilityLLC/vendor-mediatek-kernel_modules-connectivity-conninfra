411a81a598a6a22220ef5875e4792b8886f94ead
Andrew-sh.Cheng <andrew-sh.cheng@mediatek.com>
2022-01-07 01:51:38 +0800

    [ALPS06400225] conninfra: remove unused code
    Conninfra will get EMI API by
     #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
     #include <soc/mediatek/emi.h>
     #else
     #include <memory/mediatek/emi.h>
     #endif
    instead of
     #ifdef CONFIG_MTK_EMI
     #include <mt_emi_api.h>
     #endif
    
    Remove it to avoid build error, due to there is no <mt_emi_api.h>
    after kernrl-4.19
    
    MTK-Commit-Id: 1986d8e96b6fbbdf19871fae7c7143acfe7400a1
    
    CR-Id: ALPS06400225
    Feature: [Module]Conninfra Driver
    Signed-off-by: Andrew-sh.Cheng <andrew-sh.cheng@mediatek.com>
    Change-Id: Icba8aa1bd353b7c73787179fa2a3504d4974a140

--- platform/mt6893/mt6893_emi.c
+++ platform/mt6893/mt6893_emi.c
@@ -8,9 +8,6 @@
 #include <linux/version.h>
 #include <linux/memblock.h>
 #include <linux/platform_device.h>
-#ifdef CONFIG_MTK_EMI
-#include <mt_emi_api.h>
-#endif
 #include <linux/of_reserved_mem.h>
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 4, 0))
