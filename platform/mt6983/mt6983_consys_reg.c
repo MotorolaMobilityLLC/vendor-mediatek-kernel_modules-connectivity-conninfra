31cb96dd028a87dec420a78b549d87464b6e0716
Tim Chang <tim.chang@mediatek.com>
2022-01-20 11:13:04 +0800

    [ALPS06475556] conninfra: print irq status and more debug registers
    1. Print irq status while resume to find which irq wakes AP.
    2. Update registers of debug SOP
    
    MTK-Commit-Id: 2923b3d2d2ed02f54c5ee5cc8291cb106a045d8d
    
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS06475556
    Feature: [Module]Conninfra Driver
    Change-Id: I8c7ee7eec80f12dd8c7df8676b5466c8e9d47bc2

--- platform/mt6983/mt6983_consys_reg.c
+++ platform/mt6983/mt6983_consys_reg.c
@@ -18,7 +18,7 @@
 #include "osal.h"
 #include "mt6983_pmic.h"
 
-#define LOG_TMP_BUF_SZ 256
+#define CONSYS_DUMP_BUF_SIZE 512
 
 static int consys_reg_init(struct platform_device *pdev);
 static int consys_reg_deinit(void);
@@ -45,6 +45,7 @@ struct consys_reg_mng_ops g_dev_consys_reg_ops_mt6983 = {
 };
 
 static struct conn_debug_info_mt6983 *debug_info;
+static char *debug_buf;
 
 static const char* consys_base_addr_index_to_str[CONSYS_BASE_ADDR_MAX] = {
 	"infracfg_ao",
@@ -80,15 +81,16 @@ int consys_is_consys_reg(unsigned int addr)
 	return 0;
 }
 
-#define CONSYS_DUMP_BUF_SIZE 512
 static void consys_print_log(const char *title, struct conn_debug_info_mt6983 *info)
 {
-	char buf[CONSYS_DUMP_BUF_SIZE];
 	char temp[13];
 	int i;
 
+	if (debug_buf == NULL)
+		return;
+
 	temp[0] = '\0';
-	if (snprintf(buf, CONSYS_DUMP_BUF_SIZE, "%s", title) < 0) {
+	if (snprintf(debug_buf, CONSYS_DUMP_BUF_SIZE, "%s", title) < 0) {
 		pr_notice("%s snprintf failed\n", __func__);
 		return;
 	}
@@ -98,9 +100,9 @@ static void consys_print_log(const char *title, struct conn_debug_info_mt6983 *i
 			pr_notice("%s snprintf failed\n", __func__);
 			return;
 		}
-		strncat(buf, temp, strlen(temp) + 1);
+		strncat(debug_buf, temp, strlen(temp) + 1);
 	}
-	pr_info("%s\n", buf);
+	pr_info("%s\n", debug_buf);
 }
 
 static void consys_print_power_debug(int level)
@@ -210,6 +212,11 @@ static int consys_check_conninfra_on_domain(void)
 	return 1;
 }
 
+int consys_check_conninfra_on_domain_mt6983(void)
+{
+	return consys_check_conninfra_on_domain();
+}
+
 static int consys_check_conninfra_off_domain(void)
 {
 	unsigned int r;
@@ -306,7 +313,13 @@ static void consys_debug_init_mt6983(void)
 {
 	debug_info = (struct conn_debug_info_mt6983 *)osal_malloc(sizeof(struct conn_debug_info_mt6983));
 	if (debug_info == NULL) {
-		pr_notice("%s malloc failed\n", __func__);
+		pr_notice("%s debug_info malloc failed\n", __func__);
+		return;
+	}
+
+	debug_buf = osal_malloc(CONSYS_DUMP_BUF_SIZE);
+	if (debug_buf == NULL) {
+		pr_notice("%s debug_buf malloc failed\n", __func__);
 		return;
 	}
 
@@ -315,13 +328,15 @@ static void consys_debug_init_mt6983(void)
 
 static void consys_debug_deinit_mt6983(void)
 {
-	if (debug_info == NULL) {
-		pr_notice("%s debug_info is NULL\n", __func__);
-		return;
+	if (debug_info != NULL) {
+		osal_free(debug_info);
+		debug_info = NULL;
 	}
 
-	osal_free(debug_info);
-	debug_info = NULL;
+	if (debug_buf != NULL) {
+		osal_free(debug_buf);
+		debug_buf = NULL;
+	}
 
 	consys_debug_deinit_mt6983_debug_gen();
 }
