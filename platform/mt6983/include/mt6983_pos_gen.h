a75b2d1b47a94d40d866bb3b70a9a5a3696b1d7d
Tim Chang <tim.chang@mediatek.com>
2022-02-17 15:41:14 +0800

    [ALPS06198747] conninfra: mt6983: update POS
    update VS2 voter setting
    
    MTK-Commit-Id: 9196eb8d024c1aae28969faeeab682ae76f14192
    
    Signed-off-by: Tim Chang <tim.chang@mediatek.com>
    CR-Id: ALPS06198747
    Feature: [Module]Conninfra Driver
    Change-Id: I32d9c8114d0e10b9577c2dc2059fc89ec9e9b7a7

--- platform/mt6983/include/mt6983_pos_gen.h
+++ platform/mt6983/include/mt6983_pos_gen.h
@@ -11,7 +11,7 @@
  * It should not be modified by hand.
  *
  * Reference POS file,
- * - Lxxxn_power_on_sequence_20210902.xlsx
+ * - Lxxxn_power_on_sequence_20211007.xlsx
  * - Lxxxn_conn_infra_sub_task_210811.xlsx
  * - conn_infra_cmdbt_instr_autogen_20210902.txt
  */
