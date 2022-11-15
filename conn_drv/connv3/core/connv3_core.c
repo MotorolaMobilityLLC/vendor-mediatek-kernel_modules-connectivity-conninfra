f4ac16238c5dc2fceca3ec960256f89d8eec80c2
YL Wang <yl.wang@mediatek.com>
2022-11-15 20:00:27 +0800

    [ALPS07704133] connv3: remove screen on cal
    [Problem]
    Wifi on fail due to pre-cal is not finished.
    
    [Analysis]
    Screen on cal is after power on cal quickly. It changes the global variable
    about why to trigger calibration. In connac3, screen on cal is disabled.
    Hence, calibration would not be excuted.
    
    [Solution]
    Remove screen on cal directly.
    
    MTK-Commit-Id: ff9dd03a43bb63f300192f1354f113a86dc2d0f7
    
    CR-Id: ALPS07704133
    Feature: conn_infra
    Change-Id: I0dca18ffadd0eabba8d4289175bba5b298518c3b

--- conn_drv/connv3/core/connv3_core.c
+++ conn_drv/connv3/core/connv3_core.c
@@ -1420,6 +1420,7 @@ int connv3_core_pre_cal_start(void)
 
 int connv3_core_screen_on(void)
 {
+#if 0
 	int ret = 0, rst_status;
 	unsigned long flag;
 	struct connv3_ctx *ctx = &g_connv3_ctx;
@@ -1439,11 +1440,13 @@ int connv3_core_screen_on(void)
 		pr_err("[%s] send msg fail, ret = %d\n", __func__, ret);
 		return -1;
 	}
+#endif
 	return 0;
 }
 
 int connv3_core_screen_off(void)
 {
+#if 0
 	int ret = 0;
 	struct connv3_ctx *ctx = &g_connv3_ctx;
 
@@ -1453,7 +1456,7 @@ int connv3_core_screen_off(void)
 		pr_err("[%s] send msg fail, ret = %d\n", __func__, ret);
 		return -1;
 	}
-
+#endif
 	return 0;
 }
 
