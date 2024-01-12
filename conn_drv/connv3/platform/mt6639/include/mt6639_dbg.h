edbae20536a02f21df3f7501522f162de1afd322
YL Wang <yl.wang@mediatek.com>
2024-01-12 20:04:45 +0800

    [ALPS07470603] connv3: mt6639: fix dump list and enhancement
    1. Fix dump list error.
    2. Dump power and bus section B even when irq timeout is triggerred.
    
    MTK-Commit-Id: d904fa3c6366b6bbb68ec0ec8b56db59d5394cfc
    
    CR-Id: ALPS07470603
    Feature: conn_infra
    Change-Id: I4c1e680aabcba9c62b0df5b35161cde0dff4bb55

--- conn_drv/connv3/platform/mt6639/include/mt6639_dbg.h
+++ conn_drv/connv3/platform/mt6639/include/mt6639_dbg.h
@@ -14,17 +14,17 @@
 #define MT6639_CONN_INFRA_VERSION_ID		0x03010001
 #define MT6639_CONN_INFRA_OFF_IRQ_REG		0x7c023400
 
-struct connv3_dbg_command mt6639_pwr_b[] = {
+const struct connv3_dbg_command mt6639_pwr_b[] = {
 	/* Write, addr, mask, value, Read, addr*/
 	{ false, 0, 0, 0, true, 0x7C060A10},
 };
 
-struct connv3_dump_list mt6639_dmp_list_pwr_b = {
+const struct connv3_dump_list mt6639_dmp_list_pwr_b = {
 	"power_b", sizeof(mt6639_pwr_b)/sizeof(struct connv3_dbg_command),
 	mt6639_pwr_b,
 };
 
-struct connv3_dbg_command mt6639_pwr_c[] = {
+const struct connv3_dbg_command mt6639_pwr_c[] = {
 	/* Write, addr, mask, value, Read, addr*/
 	{false, 0, 0, 0, true, 0x7C011030},
 	{false, 0, 0, 0, true, 0x7C012050},
@@ -57,13 +57,13 @@ struct connv3_dbg_command mt6639_pwr_c[] = {
 	{false, 0, 0, 0, true, 0x7C098004},
 };
 
-struct connv3_dump_list mt6639_dmp_list_pwr_c = {
+const struct connv3_dump_list mt6639_dmp_list_pwr_c = {
 	"power_c", sizeof(mt6639_pwr_c)/sizeof(struct connv3_dbg_command),
 	mt6639_pwr_c,
 };
 
-struct connv3_dbg_command mt6639_bus_a[] = {
-	{true, 0x7c02340c, 0x0, 0x00000000, false, 0x0},
+const struct connv3_dbg_command mt6639_bus_a[] = {
+	{true, 0x7c023408, 0x0, 0x00000000, false, 0x0},
 	{true, 0x7c02340c, 0x0, 0x00010001, true, 0x7c023404},
 	{true, 0x7c02340c, 0x0, 0x00020001, true, 0x7c023404},
 	{true, 0x7c02340c, 0x0, 0x00030001, true, 0x7c023404},
@@ -83,12 +83,12 @@ struct connv3_dbg_command mt6639_bus_a[] = {
 	{false, 0, 0, 0, true, 0x7c00EA04},
 };
 
-struct connv3_dump_list mt6639_dmp_list_bus_a = {
+const struct connv3_dump_list mt6639_dmp_list_bus_a = {
 	"bus_a", sizeof(mt6639_bus_a)/sizeof(struct connv3_dbg_command),
 	mt6639_bus_a,
 };
 
-struct connv3_dbg_command mt6639_bus_b[] = {
+const struct connv3_dbg_command mt6639_bus_b[] = {
 	{false, 0x0, 0x0, 0x0, true, 0x7c02341c},
 	{false, 0x0, 0x0, 0x0, true, 0x7c023420},
 	{false, 0x0, 0x0, 0x0, true, 0x7c023424},
@@ -174,12 +174,12 @@ struct connv3_dbg_command mt6639_bus_b[] = {
 	{false, 0x0, 0x0, 0x0, true, 0x7c04831c},
 };
 
-struct connv3_dump_list mt6639_dmp_list_bus_b = {
+const struct connv3_dump_list mt6639_dmp_list_bus_b = {
 	"bus_b", sizeof(mt6639_bus_b)/sizeof(struct connv3_dbg_command),
 	mt6639_bus_b,
 };
 
-struct connv3_dbg_command mt6639_cfg_clk_a[] = {
+const struct connv3_dbg_command mt6639_cfg_clk_a[] = {
 	{false, 0x0, 0x0, 0x0, true, 0x7C060A00},
 	{false, 0x0, 0x0, 0x0, true, 0x7C060A0C},
 	{true, 0x7C06015c, 0x7, 0x0, true, 0x7C060a04},
@@ -198,20 +198,20 @@ struct connv3_dbg_command mt6639_cfg_clk_a[] = {
 	{true, 0x7C060160, 0xf, 0x5, true, 0x7C060a08},
 };
 
-struct connv3_dump_list mt6639_dmp_list_cfg_clk_a = {
+const struct connv3_dump_list mt6639_dmp_list_cfg_clk_a = {
 	"cfg_clk_a", sizeof(mt6639_cfg_clk_a)/sizeof(struct connv3_dbg_command),
 	mt6639_cfg_clk_a
 };
 
-struct connv3_dbg_command mt6639_cfg_clk_b[] = {
-	{false, 0x0, 0x0, 0x0, true, 0x7C023200},
-	{false, 0x0, 0x0, 0x0, true, 0x7C011130},
-	{true, 0x7C011100, 0x600000, 0x0, true, 0x7C011134},
-	{true, 0x7C011100, 0x600000, 0x200000, true, 0x7C011134},
-	{true, 0x7C011100, 0x600000, 0x400000, true, 0x7C011134},
-	{true, 0x7C011100, 0x600000, 0x600000, true, 0x7C011134},
-	{true, 0x7C01603C, 0x3f, 0x14, false, 0x0},
-	{true, 0x7C01603C, 0xfc0, 0x5c, false, 0x0},
+const struct connv3_dbg_command mt6639_cfg_clk_b[] = {
+	{false, 0x0, 0x0, 0x0, true, 0x7C023200}, /* B1 */
+	{false, 0x0, 0x0, 0x0, true, 0x7C011130}, /* B2 */
+	{true, 0x7C011100, 0x600000, 0x0, true, 0x7C011134}, /* B3 */
+	{true, 0x7C011100, 0x600000, 0x200000, true, 0x7C011134}, /* B4 */
+	{true, 0x7C011100, 0x600000, 0x400000, true, 0x7C011134}, /* B5 */
+	{true, 0x7C011100, 0x600000, 0x600000, true, 0x7C011134}, /* B6 */
+	{true, 0x7C01603C, 0x3f, 0x14, false, 0x0}, /* B7 */
+	{true, 0x7C01603C, 0xfc0, 0x5c0, false, 0x0},
 	{true, 0x7C01601C, 0x0, 0x03020100, false, 0x0},
 	{true, 0x7C016020, 0x0, 0x07060504, false, 0x0},
 	{true, 0x7C016024, 0x0, 0x0B0A0908, false, 0x0},
@@ -222,7 +222,7 @@ struct connv3_dbg_command mt6639_cfg_clk_b[] = {
 	{true, 0x7C016038, 0x0, 0x1F1E1D1C, false, 0x0},
 	{true, 0x7C016058, 0x2, 0x2, false, 0x0},
 	{false, 0x0, 0x0, 0x0, true, 0x7C023200},
-	{true, 0x7C01603C, 0x3f, 0x24, false, 0x0},
+	{true, 0x7C01603C, 0x3f, 0x24, false, 0x0}, /* B8 */
 	{true, 0x7C01601C, 0x0, 0x03020100, false, 0x0},
 	{true, 0x7C016020, 0x0, 0x07060504, false, 0x0},
 	{true, 0x7C016024, 0x0, 0x0B0A0908, false, 0x0},
@@ -235,10 +235,24 @@ struct connv3_dbg_command mt6639_cfg_clk_b[] = {
 	{false, 0x0, 0x0, 0x0, true, 0x7C023200},
 };
 
-struct connv3_dump_list mt6639_dmp_list_cfg_clk_b = {
+const struct connv3_dump_list mt6639_dmp_list_cfg_clk_b = {
 	"cfg_clk_b", sizeof(mt6639_cfg_clk_b)/sizeof(struct connv3_dbg_command),
 	mt6639_cfg_clk_b
 };
 
+/* Not in debug sop, add for extra information.
+ */
+const struct connv3_dbg_command mt6639_bus_extra[] = {
+	/* For RFSPI timeout */
+	{false, 0x0, 0x0, 0x0, true, 0x7c012038},
+	{false, 0x0, 0x0, 0x0, true, 0x7c01203c},
+	{false, 0x0, 0x0, 0x0, true, 0x7c012080},
+	{true, 0x7C023500, 0x0, 0xAEAA68, true, 0x7C023504},
+};
+
+const struct connv3_dump_list mt6639_dmp_list_bus_extra = {
+	"bus_extra", sizeof(mt6639_bus_extra)/sizeof(struct connv3_dbg_command),
+	mt6639_bus_extra
+};
 
 #endif
