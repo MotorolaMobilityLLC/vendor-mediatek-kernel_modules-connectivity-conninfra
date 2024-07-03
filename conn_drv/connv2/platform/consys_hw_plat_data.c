d9009443fd54b8cbce7965bd9f6b49d41600ebd6
Mike.Chiang <mike.chiang@mediatek.com>
2024-07-03 14:43:57 +0800

    [ALPS08932103] mt6899: conninfra pos
    [Description]
    Conninfra driver porting.
    
    [Test]
    Build pass.
    
    MTK-Commit-Id: 32b10b3bd4ca3c6c7771b381c2dbe06a81e5902d
    
    CR-Id: ALPS08932103
    Feature: conn_infra
    Change-Id: I630bb62a30ffe732e0d0b31cff322d7ca44bf631
    (cherry picked from commit 10f3b420a0911d38a89e2f5091168bc8107400e5)

--- conn_drv/connv2/platform/consys_hw_plat_data.c
+++ conn_drv/connv2/platform/consys_hw_plat_data.c
@@ -35,6 +35,7 @@ struct conninfra_plat_data __weak mt6878_6637_plat_data = {};
 struct conninfra_plat_data __weak mt6878_6631_plat_data = {};
 struct conninfra_plat_data __weak mt6878_6631_6686_plat_data = {};
 struct conninfra_plat_data __weak mt6991_plat_data = {};
+struct conninfra_plat_data __weak mt6899_plat_data = {};
 
 #ifdef CONFIG_OF
 const struct of_device_id apconninfra_of_ids[] = {
@@ -150,6 +151,12 @@ const struct of_device_id apconninfra_of_ids[] = {
 		.compatible = "mediatek,mt6991-consys",
 	#if IS_ENABLED(CONFIG_MTK_COMBO_CHIP_CONSYS_6991)
 		.data = (void*)&mt6991_plat_data,
+	#endif
+	},
+	{
+		.compatible = "mediatek,mt6899-consys",
+	#if IS_ENABLED(CONFIG_MTK_COMBO_CHIP_CONSYS_6899)
+		.data = (void*)&mt6899_plat_data,
 	#endif
 	},
 	{}
